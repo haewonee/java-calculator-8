a352e9c9f7788f2129558d8d31441f784736fcaa
haewonee <godnjs5870@naver.com>
2025-10-18 22:18:36 +0900

    feat: , 또는 : 을 기준으로 숫자 분리 및 합산 기능 구현

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -5,6 +5,12 @@ public class Calculator {
         if (input == null || input.isEmpty()){
             return 0;
         }
-        return 0;
+
+        String[] numbers = input.split("[,:]");
+        int sum = 0;
+        for (String number : numbers){
+            sum += Integer.parseInt(number);
+        }
+        return sum;
     }
 }

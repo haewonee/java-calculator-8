800aca7a0cc5efc94d02f5fa29501b67b93dfc44
haewonee <godnjs5870@naver.com>
2025-10-18 22:13:56 +0900

    feat: 빈 문자열 또는 null 입력 시 0 반환 기능 구현

---
+++ src/main/java/calculator/Calculator.java
@@ -0,0 +1,10 @@
+package calculator;
+
+public class Calculator {
+    public int add(String input){
+        if (input == null || input.isEmpty()){
+            return 0;
+        }
+        return 0;
+    }
+}

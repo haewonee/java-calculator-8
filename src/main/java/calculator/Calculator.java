a5a28266a3eb63fdb7bb39d376e497cc2c88e821
haewonee <godnjs5870@naver.com>
2025-10-18 22:31:12 +0900

    feat: 커스텀 구분자 지정 기능, 커스텀 구분자 기준으로 합산 기능 추가

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -6,11 +6,27 @@ public class Calculator {
             return 0;
         }
 
+
+        if(input.startsWith("//")){
+            String[] parts = input.split("\n");
+            String separator = parts[0].substring(2);
+            String numbers = parts[1];
+
+            String[] tokens = numbers.split(separator);
+            int sum = 0;
+            for(String token : tokens){
+                sum += Integer.parseInt(token);
+            }
+            return sum;
+        }
+
         String[] numbers = input.split("[,:]");
         int sum = 0;
         for (String number : numbers){
             sum += Integer.parseInt(number);
         }
+
+
         return sum;
     }
 }

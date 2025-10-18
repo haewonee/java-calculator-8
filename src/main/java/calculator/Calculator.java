7334c57f0cdd7860f3950c5bbf7c1416e1922ddf
haewonee <godnjs5870@naver.com>
2025-10-18 23:01:17 +0900

    fix: 콘솔 입력 시 \\n 문자열을 실제 개행 문자로 변환해 예외 방지

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,18 +1,23 @@
 package calculator;
 
+import java.util.regex.Pattern;
+
 public class Calculator {
     public int add(String input){
         if (input == null || input.isEmpty()){
             return 0;
         }
 
+        if(input.contains("\\n")) {
+            input = input.replace("\\n","\n");
+        }
 
         if (input.startsWith("//")){
             String[] parts = input.split("\n");
             String separator = parts[0].substring(2);
             String numbers = parts[1];
 
-            String[] tokens = numbers.split(separator);
+            String[] tokens = numbers.split(Pattern.quote(separator));
             int sum = 0;
             for(String token : tokens){
                 int number = parseNumber(token);
